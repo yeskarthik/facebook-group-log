8e9c4e46a2048b156f19b657c6fe11a5881c07f5
Karthik Subramanian <karthik.s.sundaram@gmail.com>
2012-09-17 23:18:19 +0530

    Kippt Sync added

--- fb_group_sync.py
+++ fb_group_sync.py
@@ -13,41 +13,44 @@ def index():
     jsondata = json.loads(data)
     for i,post in enumerate(jsondata['data']):
         print post.get('id') 
-        #if(post.get('message') !=  None):
-        #    print post.get('message')
-            #make this regex better if you want
-	#    try:
-                #urls =  re.findall("(?P<url>https?://[^\s]+)", post.get('message'))
-		#Post the urls to Kippt
-                #for url in urls:
-         #           req = urllib2.Request('')
-         #           req.add_header('X-Kippt-Username', 'yeskarthik')
-         #           req.add_header('X-Kippt-API-Token','1d3bacd51c5d0e9ecd90d527ac013b723513a48c')
-         #           req.add_header('data','{"url": '+url+', "list": "/api/lists/157407/"}')
-                    
-         #           r = urllib2.urlopen(req)
-         #   except:
-         #       pass
+        #Do the DB part here
 
-	if(post.get('link') !=  None):
+        # Post urls to Kippt
+       	if(post.get('link') !=  None):
             print post.get('link')
             print post.get('name')
             print post.get('description')
             link = post.get('link').encode("ascii","ignore")
             name = post.get('name').encode("ascii","ignore")
-            description = post.get('description').encode("ascii","ignore")
-	    #values={"url": post.get('link'), "list": "/api/lists/157407/", "title":post.get('name'),"notes":post.get('description')}
-	    values = '{"url": "'+link+'" , "list": "/api/lists/157407/", "title":"'+name+'", "notes":"'+description+'"}'
+            description = post.get('description').encode("ascii","ignore") + '\n' + post.get('from').get('name').encode("ascii","ignore")
+	    values = '{"url": "'+link+'" , "list":"'+config.get('kippt','listuri')+'", "title":"'+name+'", "notes":"'+description+ '"}'
             print values
-#	    data = urllib.urlencode(values)	
-#	    data = data.encode('utf-8')
-            req = urllib2.Request('https://kippt.com/api/clips/',values)
-            req.add_header('X-Kippt-Username', 'yeskarthik')
-            req.add_header('X-Kippt-API-Token','1d3bacd51c5d0e9ecd90d527ac013b723513a48c')
+            req = urllib2.Request(config.get('kippt','url'),values)
+            req.add_header('X-Kippt-Username', config.get('kippt','username'))
+            req.add_header('X-Kippt-API-Token', config.get('kippt','apitoken'))
 	    
             r = urllib2.urlopen(req)
 	    print r.read()
-    
+        elif(post.get('message') !=  None):
+           print post.get('message')
+            #make this regex better if you want
+	   try:
+               urls =  re.findall("(?P<url>https?://[^\s]+)", post.get('message'))
+               description = post.get('message')
+               for url in urls:
+                   description.replace(url,"")	       
+               description = description  + '\n' + post.get('from').get('name').encode("ascii","ignore")
+
+               for url in urls:
+                   values = '{"url": "'+url+'" , "list": "'+config.get('kippt','listuri')+'", "notes":"'+description+'"}' 
+                   req = urllib2.Request(config.get('kippt','url'),values)
+                   req.add_header('X-Kippt-Username', config.get('kippt','username'))
+                   req.add_header('X-Kippt-API-Token', config.get('kippt','apitoken'))
+                   r = urllib2.urlopen(req)
+           except:
+               pass
+
+
   
 if __name__ == "__main__":
     index()
