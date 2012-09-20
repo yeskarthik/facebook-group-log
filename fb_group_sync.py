bf57b31ca2bd31e6b8ef7777f62abe1847c2c253
Madhumitha Viswanathan <madhuvishy@gmail.com>
2012-09-20 22:33:18 +0530

    Added script to find first page url if needed

--- fb_group_sync.py
+++ fb_group_sync.py
@@ -4,77 +4,96 @@ import re
 import ConfigParser
 import json
 import MySQLdb
+import logging
+import traceback
+from datetime import datetime
 
 class FbGroupArchiver:
 
-    def __init__(self):
+    def __init__(self, config_filename):
         self.config = ConfigParser.ConfigParser()
-        self.config.read('properties.cfg')
+        try:
+            self.config.read(config_filename)
+        except:
+            print 'Config file'+str(config_filename)+' cannot be read'
+            return
         config = self.config
         self.db = MySQLdb.connect(user=config.get('db','user'), passwd=config.get('db','password'), db=config.get('db','name'))
         self.cursor = self.db.cursor()
+        logging.basicConfig(filename=config.get('logging','infolog'), level=logging.INFO)
 
     def process_data(self, group_url=None):
+        controlchar_regex = re.compile(r'[\n\r\t]')
         cursor = self.cursor
         config = self.config
         if(group_url == None):
             group_url = config.get('group','url')
         data = urllib2.urlopen(group_url).read()
         jsondata = json.loads(data)
-
         for i,post in enumerate(jsondata['data']):
-            print post.get('id')
-            post_id = post.get('id')
-            message = post.get('message')
-            author_name = post.get('from').get('name').encode('ascii','ignore').replace('"','')
-            author_id = post.get('from').get('id')
-            
-            if(message != None):
-                message = message.encode('ascii','ignore').replace('"','')
-                comments_count = None
-                likes_count = None
-                if(post.get('comments') != None):
-                    comments_count = post.get('comments').get('count')
-                if(post.get('likes') != None):
-                    likes_count = post.get('likes').get('count')
-                # Do the DB part here
-                cursor.execute("""SELECT InsertPost(%s, %s, %s, %s, %s, %s)""", (author_name, author_id, message, likes_count, comments_count, post_id))
-                code = cursor.fetchone()
-                # code[0] indicates the number of affected rows, if its 1 -> successful insert, if not the post already exists in thr Db
-                if(code[0] == 1 and post.get('link') !=  None):
-                    link = post.get('link').encode('ascii','ignore')
-                    title = post.get('name').encode('ascii','ignore').replace('"','')
-                    
-                    if(post.get('description') != None):
-                        description = post.get('description').encode('ascii','ignore').replace('"','') + ' - ' + author_name
-                    else:
-                        description = message + ' - ' + author_name
-            
-                    # Build the JSON
-                    values = '{"url": "'+link+'" , "list":"'+config.get('kippt','listuri')+'", "title":"'+title+'", "notes":"'+description+ '"}'
-                    r = self.post_to_kippt(values)
-                    self.post_link(r, post_id)
+            try:
+                print post.get('id')
+                post_id = post.get('id')
+                message = post.get('message')
+                author_name = post.get('from').get('name').encode('ascii','ignore').replace('"','')
+                author_id = post.get('from').get('id')
+                
+                if(message != None):
+                    message = message.encode('ascii','ignore').replace('"','')
+                    comments_count = None
+                    likes_count = None
+                    title = ''
+                    if(post.get('comments') != None):
+                        comments_count = post.get('comments').get('count')
+                    if(post.get('likes') != None):
+                        likes_count = post.get('likes').get('count')
+                    # Do the DB part here
+                    cursor.execute("""SELECT InsertPost(%s, %s, %s, %s, %s, %s)""", (author_name, author_id, message, likes_count, comments_count, post_id))
+                    code = cursor.fetchone()
+                    # code[0] indicates the number of affected rows, if its 1 -> successful insert, if not the post already exists in thr Db
+                    if(code[0] == 1 and post.get('link') !=  None):
+                        link = post.get('link').encode('ascii','ignore')
+                        if(post.get('name') != None):
+                            title = controlchar_regex.sub(' ',post.get('name').encode('ascii','ignore').replace('"',''))
+                        if(post.get('description') != None):
+                            description = post.get('description').encode('ascii','ignore').replace('"','')
+                            description = controlchar_regex.sub(' ',description) + ' - ' + author_name
+                        else:
+                            description = controlchar_regex.sub(' ',message) + ' - ' + author_name
+                
+                        # Build the JSON
+                        values = '{"url": "'+link+'" , "list":"'+config.get('kippt','listuri')+'", "title":"'+title+'", "notes":"'+description+ '"}'
+                        r = self.post_to_kippt(values)
+                        self.post_link(r, post_id)
 
-                elif(code[0] == 1):
-                   
-                   # make this regex better if you want
-                   try:
-                       urls =  re.findall("(?P<url>https?://[^\s]+)", post.get('message'))
-                       description = post.get('message').replace('"','').replace('\n', '')
-                       for url in urls:
-                           description = description.replace(url, '')
-                       description = description  + ' - ' + author_name
+                    elif(code[0] == 1):
+                       
+                       # make this regex better if you want
+                       try:
+                           description = post.get('message').replace('"','')
+                           description = controlchar_regex.sub(' ',description)
+                           urls =  re.findall("(?P<url>https?://[^\s]+)", description)
+                           for url in urls:
+                               description = description.replace(url, '')
+                           description = description  + ' - ' + author_name
 
-                       for url in urls:
-                           # Build the JSON
-                           values = '{"url": "'+url+'" , "list": "'+config.get('kippt','listuri')+'", "notes":"'+description+'"}' 
-                           r = self.post_to_kippt(values)
-                           self.post_link(r, post_id)
-                   except:
-                       pass
+                           for url in urls:
+                               # Build the JSON
+                               values = '{"url": "'+url+'" , "list": "'+config.get('kippt','listuri')+'", "notes":"'+description+'"}' 
+                               r = self.post_to_kippt(values)
+                               self.post_link(r, post_id)
+                       except Exception, err:
+                           logging.error(str(datetime.now())+" "+str(err))
+                           traceback.print_exc(file = open(config.get('logging','errorlog'),'a'))
+            except Exception, err:
+                print 'Some error'
+                logging.error(str(datetime.now())+" "+str(err))
+                traceback.print_exc(file = open(config.get('logging','errorlog'),'a'))
         print "Archiving Complete!"
+        logging.info(str(datetime.now())+' Archiving Complete for page: '+group_url)
 
     def post_to_kippt(self, values):
+        print values
         config = self.config
         req = urllib2.Request(config.get('kippt','url'),values)
         req.add_header('X-Kippt-Username', config.get('kippt','username'))
--- firstpage.py
+++ firstpage.py
@@ -8,25 +8,28 @@ from fb_group_sync import FbGroupArchiver
 
 def index():
     config = ConfigParser.ConfigParser()
-    pagedir = 'previous'
-    if(len(sys.argv)>3):
-        pagedir = sys.argv[3]
     if(len(sys.argv)>2):
         group_url = sys.argv[2]
     elif(len(sys.argv)>1):
         config.read(sys.argv[1])
         group_url = config.get('group','url')
     else:
-        print 'Usage: python batch_script.py [Config file name] [group page url(optional)] [paging direction(optional)]'
+        print 'Usage: python batch_script.py [Config file name] [group page url(optional)]'
         return
+    url = 'Not found yet'
     data = urllib2.urlopen(group_url).read()
     jsondata = json.loads(data)
+    print 'Finding first page url - Long running process. Might take several minutes.'
     while(group_url is not None):
-        FbGroupArchiver(sys.argv[1]).process_data(group_url)
         data = urllib2.urlopen(group_url).read()
         jsondata = json.loads(data)
-        group_url = jsondata.get('paging').get(pagedir)
-        print group_url
+        if(jsondata.get('paging') != None):
+            url = group_url
+            group_url = jsondata.get('paging').get('next')
+        else:
+            return
+    print url
+
 if __name__ == "__main__":
     index()
 
